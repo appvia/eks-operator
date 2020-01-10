a12ae0b3869654f4b0b39d7698185aa622724578
Daniel Whatmuff <danielwhatmuff@gmail.com>
2020-01-10 09:50:43 +0000

    adding node group types spec and crd

--- pkg/apis/aws/v1alpha1/eksnodegroup_types.go
+++ pkg/apis/aws/v1alpha1/eksnodegroup_types.go
@@ -1,26 +1,38 @@
 package v1alpha1
 
 import (
+	core "github.com/appvia/hub-apis/pkg/apis/core/v1"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // EKSNodeGroupSpec defines the desired state of EKSNodeGroup
 // +k8s:openapi-gen=true
 type EKSNodeGroupSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	AmiType string `json:"amitype,omitempty"`
+	// +kubebuilder:validation:Required
+	ClusterName        string `json:"clustername"`
+	DiskSize           string `json:"disksize,omitempty"`
+	ForceUpdateEnabled string `json:"forceupdateenabled,omitempty"`
+	InstanceTypes      string `json:"instancetypes,omitempty"`
+	Labels             string `json:"labels,omitempty"`
+	NodegroupName      string `json:"nodegroupname,omitempty"`
+	// +kubebuilder:validation:Required
+	NodeRole       string `json:"noderole"`
+	ReleaseVersion string `json:"releaseversion,omitempty"`
+	RemoteAccess   string `json:"remoteaccess,omitempty"`
+	ScalingConfig  string `json:"scalingconfig,omitempty"`
+	// +kubebuilder:validation:Required
+	Subnets string `json:"subnets"`
+	Tags    string `json:"tags,omitempty"`
+	Version string `json:"version,omitempty"`
 }
 
 // EKSNodeGroupStatus defines the observed state of EKSNodeGroup
 // +k8s:openapi-gen=true
 type EKSNodeGroupStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	// Status provides a overall status
+	Status core.Status `json:"status"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
